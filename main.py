a3d1ddc5f5e8d1fe1b393772e89448d2129234f1
nickkaparinos <nickkaparinos@gmail.com>
2022-01-22 19:12:42 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+"""
+Natural Language Processing with Disaster Tweets
+Kaggle competition
+Nick Kaparinos
+2022
+"""
+
+from utilities import *
+import pandas as pd
+import time
+from nltk.tokenize import TweetTokenizer
+from sklearn.model_selection import StratifiedKFold
+from transformers import BertTokenizer
+import torch
+
+
+def main():
+    start = time.perf_counter()
+    seed = 0
+    set_all_seeds(seed)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = 'cpu'
+
+    # Read data
+    train_data = pd.read_csv('Data/train.csv')
+    train_data = train_data.iloc[:25]
+    X_train = train_data.iloc[:, :-1]
+    y_train = train_data.iloc[:, -1]
+    skf = StratifiedKFold(n_splits=4)
+
+    # Wandb
+    project = 'Kaggle-Disaster'
+    name = 'tasos'
+    notes = ''
+    config = {}
+    wandb.init(project=project, entity="nickkaparinos", name=name, config=config, notes=notes, group='', reinit=True)
+
+    for train_index, validation_index in skf.split(X_train, y_train):
+        # Split
+        X_train_fold, y_train_fold = train_data.iloc[train_index], y_train.iloc[train_index]
+        X_val_fold, y_val_fold = train_data.iloc[validation_index], y_train.iloc[validation_index]
+
+        train_dataset, validation_dataset = Dataset(X_train_fold, y_train_fold), Dataset(X_val_fold, y_val_fold)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
+        validation_dataloader = torch.utils.data.DataLoader(validation_dataset, batch_size=2)
+
+        # Model
+        model = BertClassifier().to(device)
+        loss_fn = torch.nn.NLLLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+        epochs = 1
+        fold = 0
+
+        for epoch in range(epochs):
+            print(f'{epoch = }')
+            train_one_epoch(model, train_dataloader, loss_fn, optimizer, epoch + 1, fold, device)
+
+        # fet = train_dataset[0]
+        #
+        # mask = fet[0]['attention_mask'].to(device)
+        # input_id = fet[0]['input_ids'].squeeze(1).to(device)
+        #
+        # output = model(input_id, mask)
+
+        fet = 5
+
+    # Execution Time
+    end = time.perf_counter()
+    print(f"\nExecution time = {end - start:.2f} second(s)")
+
+
+if __name__ == '__main__':
+    main()
---
+++ utilities.py
@@ -0,0 +1,122 @@
+"""
+Natural Language Processing with Disaster Tweets
+Kaggle competition
+Nick Kaparinos
+2022
+"""
+
+import random
+import wandb
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+from torch import nn
+from transformers import BertModel
+from transformers import BertTokenizer
+from sklearn.metrics import accuracy_score, f1_score
+from transformers import logging
+
+logging.set_verbosity_warning()
+
+
+labels = {'No Disaster': 0, 'Disaster': 1}
+
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, df, y):
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+        self.labels = y
+        self.texts = [self.tokenizer(text, padding='max_length', max_length=32, truncation=True, return_tensors="pt")
+                      for text in df['text']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        return np.array(self.labels.iloc[idx])
+
+    def get_batch_texts(self, idx):
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+        return batch_texts, batch_y
+
+
+class BertClassifier(nn.Module):
+
+    def __init__(self, dropout=0.5):
+        super(BertClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 5)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+
+
+def train_one_epoch(model, dataloader, loss_fn, optimizer, epoch, fold, device):
+    running_loss = 0.
+
+    model.train()
+    y_train = np.empty((0,))
+    y_pred = np.empty((0,))
+    for batch_num, batch in enumerate(tqdm(dataloader)):
+        x_batch, y_batch = batch[0], batch[1].to(device)
+
+        mask = x_batch['attention_mask'].to(device)
+        input_id = x_batch['input_ids'].squeeze(1).to(device)
+
+        # Inference
+        output = model(input_id, mask)
+        y_pred_batch = torch.argmax(output, dim=1).numpy()
+
+        #  Loss
+        optimizer.zero_grad()
+        loss = loss_fn(output, y_batch)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if batch_num % 10 == 9:
+            avg_loss = running_loss / 10
+            running_loss = 0.
+            wandb.log(data={'Epoch': epoch, f'Training_loss_{fold}': avg_loss})
+
+        # Stack
+        y_train = np.hstack([y_train, y_batch.numpy()]) if y_train.size else y_batch.numpy()
+        y_pred = np.hstack([y_pred, y_pred_batch]) if y_pred.size else y_pred_batch
+
+    # Training and Validation Metrics
+    train_accuracy = accuracy_score(y_train, y_pred)
+    train_f1 = f1_score(y_train, y_pred, average='micro')
+
+    wandb.log(data={'Epoch': epoch, 'Training_accuracy': train_accuracy, 'Training_f1': train_f1})
+
+    return train_accuracy
+
+def evaluation():
+    pass
+
+
+def set_all_seeds(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
